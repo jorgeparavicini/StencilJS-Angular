09ee2000d63e67f6d6c8f777af6e98e701f0d666
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-10 12:45:49 +0100

    Create static header

---
+++ Angular/src/app/components/static-header/static-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'static-header',
+  templateUrl: './static-header.component.html',
+  styleUrls: ['./static-header.component.scss'],
+})
+export class StaticHeaderComponent {}
