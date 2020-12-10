f869c22d46a71864b9de2b8222de58c31179ec89
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-10 13:41:28 +0100

    Create Gallery

--- Angular/src/app/components/gallery/gallery.component.ts
+++ Angular/src/app/components/gallery/gallery.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { GalleryService } from './gallery.service';
+import { GalleryEntry } from './models/gallery-entry.interface';
 
 @Component({
-    selector: 'gallery',
-    templateUrl: './gallery.component.html',
-    styleUrls: ['./gallery.component.scss']
+  selector: 'gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss'],
 })
-export class GalleryComponent {}
+export class GalleryComponent implements OnInit {
+  gallery?: GalleryEntry[];
+
+  constructor(private galleryService: GalleryService) {}
+
+  public ngOnInit(): void {
+    this.galleryService
+      .getGalleryEntries()
+      .subscribe((entries: GalleryEntry[]) => (this.gallery = entries));
+  }
+}
--- Angular/src/app/components/gallery/gallery.service.ts
+++ Angular/src/app/components/gallery/gallery.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GalleryEntry } from './models/gallery-entry.interface';
 
-const URL = "";
+const URL = "https://raw.githubusercontent.com/jorgeparavicini/StencilJS-Angular/GalleryComponent/Angular/src/assets/gallery_db.json";
 
 @Injectable({
   providedIn: 'root',
