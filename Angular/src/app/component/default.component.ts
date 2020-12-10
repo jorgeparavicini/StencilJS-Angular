1df08e5ae79677520c6cf64ddde2c6fd411ccfb9
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-10 12:39:00 +0100

    delete default component

--- Angular/src/app/component/default.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'default-component',
-  templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss'],
-})
-export class DefaultComponent {}
