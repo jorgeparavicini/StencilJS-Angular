44e9d3d8607105c9aa0e8f4ee8d7290db00036d4
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-17 13:07:22 +0100

    add render timer to stencil

--- StencilJS/src/app/components/gallery-wrapper/gallery-wrapper.component.ts
+++ StencilJS/src/app/components/gallery-wrapper/gallery-wrapper.component.ts
@@ -1,7 +1,12 @@
+import { getStartTime } from 'src/timer';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'gallery-wrapper',
-  template: '<gallery-component>',
+  template: '<gallery-component [startTime]="getStartTime()"></gallery-component>',
 })
-export class GalleryWrapperComponent {}
+export class GalleryWrapperComponent {
+  public getStartTime(): number {
+    return getStartTime();
+  }
+}
