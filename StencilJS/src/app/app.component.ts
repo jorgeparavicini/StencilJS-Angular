39bee4f51a23c7335a06eecac3f6890d74aa07f2
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-17 12:20:46 +0100

    Add render time to stencil

--- StencilJS/src/app/app.component.ts
+++ StencilJS/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { getStartTime } from 'src/timer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'StencilJS';
+
+  public getStartTime(): number {
+    return getStartTime();
+  }
 }
