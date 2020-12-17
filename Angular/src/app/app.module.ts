d3caae9a8c8f43b6906d2cdc9d2d5a7f6ab6011c
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-17 12:08:04 +0100

    Add Render Timer to angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StaticHeaderComponent } from './components/static-header/static-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StaticHeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, StaticHeaderComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Angular/src/app/components/static-header/static-header.component.ts
+++ Angular/src/app/components/static-header/static-header.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'static-header',
   templateUrl: './static-header.component.html',
   styleUrls: ['./static-header.component.scss'],
 })
-export class StaticHeaderComponent {}
+export class StaticHeaderComponent implements AfterViewChecked {
+  @Input() startTime: number = 0;
+  private isFirstCheck = true;
+
+  public ngAfterViewChecked(): void {
+    if (this.isFirstCheck) {
+      this.isFirstCheck = false;
+    } else {
+      console.log(`Render Time ${performance.now() - this.startTime}`);
+    }
+  }
+}
