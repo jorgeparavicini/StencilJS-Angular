2d14d7b149afb0a94c5f44ecdfbe70b39272de49
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-17 12:50:17 +0100

    Add Render Timer to stencil

--- StencilJS/src/app/components/home-wrapper/home-wrapper.component.ts
+++ StencilJS/src/app/components/home-wrapper/home-wrapper.component.ts
@@ -1,7 +1,12 @@
+import { getStartTime } from 'src/timer';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'home-wrapper',
-  template: '<home-component>',
+  template: '<home-component [startTime]="getStartTime()"></home-component>',
 })
-export class HomeWrapperComponent {}
+export class HomeWrapperComponent {
+  public getStartTime() {
+    return getStartTime();
+  }
+}
