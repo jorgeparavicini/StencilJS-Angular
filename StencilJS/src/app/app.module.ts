b13a374a17676c7c620e0da13ae16df6f0f1ab91
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-18 16:05:32 +0100

    remove wrapper components

--- StencilJS/src/app/app.module.ts
+++ StencilJS/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { GalleryWrapperComponent } from './components/gallery-wrapper/gallery-wrapper.component';
-import { HomeWrapperComponent } from './components/home-wrapper/home-wrapper.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeWrapperComponent },
-  { path: 'gallery', component: GalleryWrapperComponent },
+  { path: 'home', component: AppComponent },
+  { path: 'gallery', component: AppComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeWrapperComponent, GalleryWrapperComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
--- StencilJS/src/app/components/home-wrapper/home-wrapper.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'home-wrapper',
-  template: '<home-component>',
-})
-export class HomeWrapperComponent {}
