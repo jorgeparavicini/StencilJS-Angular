950a660c4d92388266e4f3f058a7642d61e85528
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-12-18 15:57:24 +0100

    remove wrapper components for stencil

--- StencilJS/src/app/app.module.ts
+++ StencilJS/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeWrapperComponent } from './components/home-wrapper/home-wrapper.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeWrapperComponent },
+  { path: 'home', component: AppComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeWrapperComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
